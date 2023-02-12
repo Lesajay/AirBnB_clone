47957b5cb1cd20170346ccdcea5f9882b791d594
RodneyRKanjanda <rodneykanjanda@gmail.com>
2023-02-12 04:12:11 -0800

    models

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@authors: Rodney Kanjanda
+          Craig Mazwi
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
